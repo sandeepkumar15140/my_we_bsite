faae579e514d02a6a1a2e8a2e00f781e8cc15055
Sandeep <sandeep.kumar15140@gmail.com>
2019-05-19 19:52:40 +0530

    Added home page in jobs templates

--- src/website_project/website_project/urls.py
+++ src/website_project/website_project/urls.py
@@ -18,12 +18,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('us/', views.us),
+    path('', jobs.views.home, name='home'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
