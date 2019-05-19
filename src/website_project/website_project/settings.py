f1be6feca72c2c9cbf3e1b08d11093b5d8314771
Sandeep <sandeep.kumar15140@gmail.com>
2019-05-19 19:12:19 +0530

    Added model for blog post

--- src/website_project/website_project/settings.py
+++ src/website_project/website_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
